6f128a658db0cf7d8edb431e8c88102e91f9f405
Patrick Connolly <patrick.c.connolly@gmail.com>
2025-01-09 14:52:03 -0800

    Use other sample data, as appears to be a glitch in data for UBI.

--- scripts/download_sample_data.sh
+++ scripts/download_sample_data.sh
@@ -1,12 +1,12 @@
-curl https://pol.is/api/v3/participationInit?conversation_id=4yy3sh84js > sample_data/participationInit.json
-curl https://pol.is/api/v3/comments?conversation_id=4yy3sh84js&moderation=true&include_voting_patterns=true > sample_data/comments.json
-curl https://pol.is/api/v3/math/pca2?conversation_id=4yy3sh84js > sample_data/math-pca2.json
+curl https://pol.is/api/v3/participationInit?conversation_id=3ntrtcehas > sample_data/participationInit.json
+curl https://pol.is/api/v3/comments?conversation_id=3ntrtcehas&moderation=true&include_voting_patterns=true > sample_data/comments.json
+curl https://pol.is/api/v3/math/pca2?conversation_id=3ntrtcehas > sample_data/math-pca2.json
 
 # We then need to manually download vote data for each participant ID.
-# Example: https://pol.is/api/v3/votes?conversation_id=4yy3sh84js&pid=0
+# Example: https://pol.is/api/v3/votes?conversation_id=3ntrtcehas&pid=0
 #
 # We can do this and aggregate into a single file via polis-exporter's makeData.js script.
 # See: https://github.com/patcon/polis-exporter
 #
-#   $ node getData.js 4yy3sh84js
-#   $ cp data/4yy3sh84js--votes.json ~/red-dwarf/samepl_data/votes.json
+#   $ node getData.js 3ntrtcehas
+#   $ cp data/3ntrtcehas--votes.json ~/red-dwarf/samepl_data/votes.json
