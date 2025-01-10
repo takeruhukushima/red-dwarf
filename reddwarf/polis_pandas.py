567d62e03967d02b88a86945a3a07a4a4bef24a5
Patrick Connolly <patrick.c.connolly@gmail.com>
2025-01-10 13:01:35 -0800

    Created new client using pandas, just to see how different. All tests passing.

---
+++ reddwarf/polis_pandas.py
@@ -0,0 +1,130 @@
+import pandas as pl # For sake of clean diffs
+from collections import defaultdict
+from sklearn.impute import SimpleImputer
+
+class PolisClient():
+    def __init__(self) -> None:
+        self.n_components = 2
+        # Ref: https://hyp.is/8zUyWM5fEe-uIO-J34vbkg/gwern.net/doc/sociology/2021-small.pdf
+        self.min_votes = 7
+        self.votes = []
+        # Ref: https://hyp.is/MV0Iws5fEe-k9BdY6UR1VQ/gwern.net/doc/sociology/2021-small.pdf
+        self.vote_matrix = None
+        # Ref: https://gist.github.com/patcon/fd9079a5fbcd533160f8ae211e975307#file-math-pca2-json-L2
+        # Ref: https://github.com/compdemocracy/polis/blob/6d04f4d144adf9640fe49b8fbaac38943dc11b9a/math/src/polismath/math/conversation.clj#L217-L225
+        self.user_vote_counts = defaultdict(int)
+        self.meta_tids = []
+        self.mod_in = []
+        self.mod_out = []
+        self.last_vote_timestamp = 0
+        self.group_clusters = []
+        self.base_clusters = {}
+        # TODO: Make accessor methods for these?
+        self.statement_count = None
+        self.participant_count = None
+
+    def get_participant_row_mask(self):
+        raise NotImplementedError
+
+    def get_statement_col_mask(self):
+        raise NotImplementedError
+
+    def apply_masks(self, participant_rows=True, statement_cols=True):
+        raise NotImplementedError
+
+    def add_votes_batch(self, votes_df):
+        """Add multiple votes from a DataFrame"""
+        # TODO: Use tuples instead of named columns later, for perf.
+        for _, row in votes_df.iterrows():
+            self.add_vote(row)
+
+    def add_vote(self, vote_row):
+        """Add a single vote to the system"""
+        # If this is a new vote (not an update)
+        self.user_vote_counts[vote_row['pid']] += 1
+
+        if self.last_vote_timestamp < int(vote_row['modified']):
+            self.last_vote_timestamp = int(vote_row['modified'])
+
+        self.votes.append({
+            'participant_id': vote_row['pid'],
+            'statement_id': vote_row['tid'],
+            'vote': vote_row['vote'],
+        })
+        # Matrix is now stale
+        self.matrix = None
+        self.statement_count = None
+        self.participant_count = None
+
+    def get_user_vote_counts(self):
+        return self.user_vote_counts
+
+    def get_meta_tids(self):
+        return self.meta_tids
+
+    def get_mod_in(self):
+        return self.mod_in
+
+    def get_mod_out(self):
+        return self.mod_out
+
+    def get_last_vote_timestamp(self):
+        return self.last_vote_timestamp
+
+    def get_group_clusters(self):
+        return self.group_clusters
+
+    # TODO: Refactor this to "process_matrix"?
+    def get_matrix(self):
+        if self.matrix is None:
+            # Only generate matrix when needed.
+            self.matrix = pl.DataFrame.from_dict(self.votes)
+            self.matrix = self.matrix.pivot(
+                values="vote",
+                index="participant_id",
+                columns="statement_id",
+            )
+
+            self.statement_count = len(self.matrix.columns)
+            self.participant_count = len(self.matrix)
+
+        return self.matrix
+
+    # Ref: https://hyp.is/8zUyWM5fEe-uIO-J34vbkg/gwern.net/doc/sociology/2021-small.pdf
+    def impute_missing_votes(self):
+        mean_imputer = SimpleImputer(missing_values=float('nan'), strategy='mean')
+        self.matrix = pl.DataFrame(
+            mean_imputer.fit_transform(self.matrix),
+        )
+
+    def load_data(self, filepath):
+        if filepath.endswith("votes.json"):
+            self.load_votes_data(filepath)
+        elif filepath.endswith("comments.json"):
+            self.load_comments_data(filepath)
+        else:
+            raise ValueError("Unknown file type")
+
+    def load_votes_data(self, filepath):
+        votes_df = pl.read_json(filepath, dtype={'modified': 'int64'})
+        # Convert pandas timestamp (nanoseconds) into unix time (milliseconds).
+        # See: https://stackoverflow.com/a/52450463
+        votes_df['modified'] = [(t // 10**6) for t in votes_df['modified']]
+        self.add_votes_batch(votes_df)
+
+    def load_comments_data(self, filepath):
+        comments_df = pl.read_json(filepath)
+        for _, row in comments_df.iterrows():
+            # TODO: Why does is_meta make this mod-in.
+            # Maybe I don't understand what mod-in does...
+            # Note: mod-in/mod-out doesn't seem to be actually used in the front-end, so a bug here wouldn't matter.
+            # Ref: https://github.com/compdemocracy/polis/blob/6d04f4d144adf9640fe49b8fbaac38943dc11b9a/math/src/polismath/math/conversation.clj#L825-L842
+            if row['is_meta'] or row['mod'] == 1:
+                self.mod_in.append(row['tid'])
+
+            if row['is_meta'] or row['mod'] == -1:
+                self.mod_out.append(row['tid'])
+
+            # Ref: https://github.com/compdemocracy/polis/blob/6d04f4d144adf9640fe49b8fbaac38943dc11b9a/math/src/polismath/math/conversation.clj#L843-L850
+            if row['is_meta']:
+                self.meta_tids.append(row['tid'])
---
+++ tests/test_polis_pandas.py
@@ -0,0 +1,104 @@
+import json
+from reddwarf.polis_pandas import PolisClient
+
+def test_user_vote_counts():
+    # Load the expected data from the JSON file
+    with open('sample_data/math-pca2.json', 'r') as file:
+        expected_data = json.load(file)['user-vote-counts']
+        # Convert keys from string to int. Not sure why API returns them as strings.
+        expected_data = {int(k): v for k,v in expected_data.items()}
+
+    # Instantiate the PolisClient and load raw data
+    client = PolisClient()
+    client.load_data('sample_data/votes.json')
+
+    # Call the method and assert the result matches the expected data
+    assert client.get_user_vote_counts() == expected_data
+
+def test_meta_tids():
+    with open('sample_data/math-pca2.json', 'r') as file:
+        expected_data = json.load(file)['meta-tids']
+
+    client = PolisClient()
+    client.load_data('sample_data/comments.json')
+
+    assert client.get_meta_tids() == expected_data
+
+def test_mod_in():
+    with open('sample_data/math-pca2.json', 'r') as file:
+        expected_data = json.load(file)['mod-in']
+
+    client = PolisClient()
+    client.load_data('sample_data/comments.json')
+
+    assert client.get_mod_in() == sorted(expected_data)
+
+def test_mod_out():
+    with open('sample_data/math-pca2.json', 'r') as file:
+        expected_data = json.load(file)['mod-out']
+
+    client = PolisClient()
+    client.load_data('sample_data/comments.json')
+
+    assert sorted(client.get_mod_out()) == sorted(expected_data)
+
+def test_last_vote_timestamp():
+    with open('sample_data/math-pca2.json', 'r') as file:
+        expected_data = json.load(file)['lastVoteTimestamp']
+
+    client = PolisClient()
+    client.load_data('sample_data/votes.json')
+
+    assert client.get_last_vote_timestamp() == expected_data
+
+SHAPE_AXIS = { 'row': 0, 'column': 1 }
+
+def test_participant_count():
+    with open('sample_data/math-pca2.json', 'r') as file:
+        expected_data = json.load(file)['n']
+
+    client = PolisClient()
+    client.load_data('sample_data/votes.json')
+    client.get_matrix()
+
+    assert client.participant_count == expected_data
+
+def test_statement_count():
+    with open('sample_data/math-pca2.json', 'r') as file:
+        expected_data = json.load(file)['n-cmts']
+
+    client = PolisClient()
+    client.load_data('sample_data/votes.json')
+    client.get_matrix()
+
+    assert client.statement_count == expected_data
+
+def test_impute_missing_values():
+    client = PolisClient()
+    client.load_data('sample_data/votes.json')
+    matrix_with_missing = client.get_matrix()
+    client.impute_missing_votes()
+    matrix_without_missing = client.get_matrix()
+
+    assert matrix_with_missing.isnull().values.sum() > 0
+    assert matrix_without_missing.isnull().values.sum() == 0
+    assert matrix_with_missing.shape == matrix_without_missing.shape
+
+# def test_group_cluster_count():
+#     with open('sample_data/math-pca2.json', 'r') as file:
+#         expected_data = json.load(file)['group-clusters']
+
+#     client = PolisClient()
+#     client.load_data('sample_data/votes.json')
+
+#     assert len(client.get_group_clusters()) == len(expected_data)
+
+# def test_pca_base_cluster_count():
+#     with open('sample_data/math-pca2.json', 'r') as file:
+#         expected_data = json.load(file)['base-clusters']
+
+#     client = PolisClient()
+#     client.load_data('sample_data/votes.json')
+
+#     assert len(client.base_clusters.get('x', [])) == len(expected_data['x'])
+#     assert len(client.base_clusters.get('y', [])) == len(expected_data['y'])
