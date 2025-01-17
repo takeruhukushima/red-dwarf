fdc580c602a4d902022e7c20c8021c48bef2bea3
Patrick Connolly <patrick.c.connolly@gmail.com>
2025-01-17 12:15:48 -0800

    Added a cache for requests to ensure tests don't get flagged as spam.

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -1,11 +1,12 @@
 import pytest
 from reddwarf.data_loader import Loader
+import requests_cache
+requests_cache.install_cache('test_cache')
 
 SMALL_CONVO_ID = "9knpdktubt"
 
 def test_load_data_from_api_comments():
     loader = Loader(conversation_id=SMALL_CONVO_ID)
-    print(loader.comments_data)
     assert len(loader.comments_data) > 0
 
 @pytest.mark.skip(reason="not ready yet")
