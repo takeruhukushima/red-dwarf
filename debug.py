409d8331eb49693268150d15998cdc556a943604
Patrick Connolly <patrick.c.connolly@gmail.com>
2025-01-23 15:35:29 -0800

    Updated debug.py a bit.

--- debug.py
+++ debug.py
@@ -1,30 +1,10 @@
 from reddwarf.polis_pandas import PolisClient
-import numpy as np
 
-client = PolisClient(is_strict_moderation=False)
-client.load_data('sample_data/votes.json')
-client.load_data('sample_data/comments.json')
-print('loading matrix...')
-# print(client.get_matrix())
-# print(client.get_active_statement_ids())
-# print(client.get_matrix()[53])
-# client.matrix = None
-# print(client.get_matrix(is_filtered=True)[53].sum())
-# print(client.get_matrix(is_filtered=True)[53].isna().sum())
-# print(client.get_matrix(is_filtered=True)[53].isna().all())
-# client.impute_missing_votes()
-# print(client.get_matrix().columns.max())
-# print(client.get_matrix().index.max())
-client.get_matrix(is_filtered=True)
-client.impute_missing_votes()
-client.run_pca()
-client.generate_figure()
-client.scale_pca_polis()
-client.generate_figure()
-# print(client.get_matrix(is_filtered=False))
-
-# print('inputing...')
-# client.impute_missing_votes()
-# print(client.matrix)
-# print(client.matrix)
-# print(np.round(client.matrix, decimals=2))
+if True:
+    client = PolisClient()
+    client.load_data(report_id="r8xhmkwp6shm9yfermteh")
+    client.get_matrix(is_filtered=False)
+    client.impute_missing_votes()
+    client.run_pca()
+    client.scale_pca_polis()
+    client.generate_figure()
