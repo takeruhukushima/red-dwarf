28f14fefa63ddb397c8fd02f1fdc3ff78ba28c65
Patrick Connolly <patrick.c.connolly@gmail.com>
2025-01-26 12:12:33 -0800

    Fixed TypeVar issue to avoid lint warnings.

--- reddwarf/models.py
+++ reddwarf/models.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, NonNegativeInt, Field, AliasChoices, field_serializer
-from typing import Literal, Optional, NewType, Annotated
+from typing import Literal, Optional, TypeVar, Annotated
 from datetime import datetime
 from enum import IntEnum
 
@@ -13,7 +13,7 @@ class ModeratedEnum(IntEnum):
     UNMODERATED = 0
     REJECTED = -1
 
-IncrementingId = NewType('IncrementingId', NonNegativeInt)
+IncrementingId = TypeVar('IncrementingId', NonNegativeInt)
 
 class Vote(BaseModel):
     @field_serializer('modified')
