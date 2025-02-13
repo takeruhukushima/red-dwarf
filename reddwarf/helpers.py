948e417195dc75916b56abf2e064d3ab7397f0c0
Patrick Connolly <patrick.c.connolly@gmail.com>
2025-02-13 12:52:07 -0800

    Improved doc for CachedLimiterSession.

--- reddwarf/helpers.py
+++ reddwarf/helpers.py
@@ -8,6 +8,8 @@ class CachedLimiterSession(CacheMixin, LimiterMixin, Session):
     """
     Session class with caching and rate-limiting behavior. Accepts arguments for both
     LimiterSession and CachedSession.
+
+    See: See: https://requests-cache.readthedocs.io/en/stable/user_guide/compatibility.html#requests-ratelimiter
     """
 
 class CloudflareBypassHTTPAdapter(HTTPAdapter):
