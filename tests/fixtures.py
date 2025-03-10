16665cac1c5e536a4a49a7f4c0a8ba6a0b76a51f
Patrick Connolly <patrick.c.connolly@gmail.com>
2025-03-10 17:01:36 -0400

    Moved representativeness tests into own file. Extracted shared fixtures.

---
+++ tests/fixtures.py
@@ -0,0 +1,11 @@
+import pytest
+import json
+
+@pytest.fixture
+def small_convo_math_data():
+    path = "tests/fixtures/below-100-ptpts"
+    filename = "math-pca2.json"
+    with open(f"{path}/{filename}", 'r') as f:
+        data = json.load(f)
+
+    return data, path, filename
