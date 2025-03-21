987c5f8a62c9adf383f85165a9344033f00030d5
Patrick Connolly <patrick.c.connolly@gmail.com>
2025-03-21 17:30:00 -0400

    Add tests for agora.run_clustering() options.

--- tests/test_agora.py
+++ tests/test_agora.py
@@ -1,19 +1,25 @@
 from tests.fixtures import small_convo_math_data, medium_convo_math_data
-from reddwarf.agora import run_clustering_v1, Conversation
+from reddwarf import agora
 from reddwarf.polis import PolisClient
 
-def test_run_clustering_real_data_small(small_convo_math_data):
-    expected_cluster_sizes = [4, 10, 5]
-    _, data_path, *_ = small_convo_math_data
+# A helper to generate votes list.
+def build_votes(data_fixture):
+    _, data_path, *_ = data_fixture
 
     client = PolisClient()
     client.load_data(filepaths=[f"{data_path}/votes.json"])
 
-    convo: Conversation = {
+    return client.data_loader.votes_data
+
+def test_run_clustering_real_data_small(small_convo_math_data):
+    expected_cluster_sizes = [4, 10, 5]
+    votes = build_votes(small_convo_math_data)
+
+    convo: agora.Conversation = {
         "id": "dummy",
-        "votes": client.data_loader.votes_data, # type:ignore
+        "votes": votes, # type:ignore
     }
-    results = run_clustering_v1(conversation=convo)
+    results = agora.run_clustering_v1(conversation=convo)
 
     assert len(expected_cluster_sizes) == len(results["clusters"])
 
@@ -23,19 +29,45 @@ def test_run_clustering_real_data_small(small_convo_math_data):
 
 def test_run_clustering_real_data_medium(medium_convo_math_data):
     expected_cluster_sizes = [60, 39, 37, 37, 3]
-    _, data_path, *_ = medium_convo_math_data
+    votes = build_votes(medium_convo_math_data)
 
-    client = PolisClient()
-    client.load_data(filepaths=[f"{data_path}/votes.json"])
-
-    convo: Conversation = {
+    convo: agora.Conversation = {
         "id": "dummy",
-        "votes": client.data_loader.votes_data, # type:ignore
+        "votes": votes, # type:ignore
     }
-    results = run_clustering_v1(conversation=convo)
+    results = agora.run_clustering_v1(conversation=convo)
 
     assert len(expected_cluster_sizes) == len(results["clusters"])
 
     for cluster_id, expected_cluster_size in enumerate(expected_cluster_sizes):
         actual_cluster_size = len(results["clusters"][cluster_id]["participants"])
         assert actual_cluster_size == expected_cluster_size
+
+def test_run_clustering_real_data_no_min_threshold_option(small_convo_math_data):
+    expected_cluster_sizes = [24, 7, 4]
+    votes = build_votes(small_convo_math_data)
+
+    convo: agora.Conversation = {
+        "id": "dummy",
+        "votes": votes, # type:ignore
+    }
+    results = agora.run_clustering_v1(conversation=convo, options={"min_user_vote_threshold": 0})
+
+    assert len(expected_cluster_sizes) == len(results["clusters"])
+
+    for cluster_id, expected_cluster_size in enumerate(expected_cluster_sizes):
+        actual_cluster_size = len(results["clusters"][cluster_id]["participants"])
+        assert actual_cluster_size == expected_cluster_size
+
+def test_run_clustering_real_data_max_clusters_option(medium_convo_math_data):
+    max_cluster_count = 3
+    expected_cluster_count = max_cluster_count
+    votes = build_votes(medium_convo_math_data)
+
+    convo: agora.Conversation = {
+        "id": "dummy",
+        "votes": votes, # type:ignore
+    }
+    results = agora.run_clustering_v1(conversation=convo, options={"max_clusters": max_cluster_count})
+
+    assert expected_cluster_count == len(results["clusters"])
