241d7a510cc5ba264cdd2bac6ab311ba3927ff1e
Patrick Connolly <patrick.c.connolly@gmail.com>
2025-04-11 18:42:32 -0400

    Rename generate_figure() arg to cluster_labels for clarity.

--- reddwarf/data_presenter.py
+++ reddwarf/data_presenter.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 def generate_figure(
         coord_dataframe: pd.DataFrame,
-        labels: Optional[List[int]] = None,
+        cluster_labels: Optional[List[int]] = None,
         flip_x: bool = False,
         flip_y: bool = False,
 ) -> None:
@@ -17,7 +17,7 @@ def generate_figure(
     Generates a matplotlib scatterplot with optional bounded clusters.
 
     The plot is drawn from a dataframe of xy values, each point labelled by index `participant_id`.
-    When a list of labels are supplied (corresponding to each row), concave hulls are drawn around them.
+    When a list of cluster labels are supplied (corresponding to each row), concave hulls are drawn around them.
 
     Signs of PCA projection coordinates are arbitrary, and can flip without
     meaning. Inverting axes can help compare results with Polis platform
@@ -25,7 +25,7 @@ def generate_figure(
 
     Args:
         coord_dataframe (pd.DataFrame): A dataframe of coordinates with columns named `x` and `y`, indexed by `participant_id`.
-        labels (List[int]): A list of labels, one for each row in `coord_dataframe`.
+        cluster_labels (List[int]): A list of group labels, one for each row in `coord_dataframe`.
         flip_x (bool): Flip the presentation of the X-axis so it descends left-to-right
         flip_y (bool): Flip the presentation of the Y-axis so it descends top-to-bottom
 
@@ -41,7 +41,7 @@ def generate_figure(
     if flip_y:
         plt.gca().invert_yaxis()
 
-    # Label points with participant_id if no labels set.
+    # Label points with participant_id
     for participant_id, row in coord_dataframe.iterrows():
         plt.annotate(str(participant_id),
             (float(row["x"]), float(row["y"])),
@@ -54,15 +54,15 @@ def generate_figure(
     scatter_kwargs["y"] = coord_dataframe.loc[:,"y"]
     scatter_kwargs["s"] = 10       # point size
     scatter_kwargs["alpha"] = 0.8  # point transparency
-    if labels is not None:
+    if cluster_labels is not None:
         # Ref: https://matplotlib.org/stable/users/explain/colors/colormaps.html#qualitative
-        scatter_kwargs["cmap"] = "Set1"    # color map
-        scatter_kwargs["c"] = labels        # color indexes
+        scatter_kwargs["cmap"] = "Set1"      # color map
+        scatter_kwargs["c"] = cluster_labels # color indexes
 
         print("Calculating convex hulls around clusters...")
-        unique_labels = set(labels)
+        unique_labels = set(cluster_labels)
         for label in unique_labels:
-            points_df = coord_dataframe[labels == label]
+            points_df = coord_dataframe[cluster_labels == label]
             print(f"Hull {str(label)}, bounding {len(points_df)} points")
             if len(points_df) < 3:
                 # TODO: Accomodate 2 points like Polis platform does.
@@ -82,8 +82,8 @@ def generate_figure(
     scatter = plt.scatter(**scatter_kwargs)
 
     # Add a legend if labels are provided
-    if labels is not None:
-        plt.colorbar(scatter, label="Cluster", ticks=labels)
+    if cluster_labels is not None:
+        plt.colorbar(scatter, label="Cluster", ticks=cluster_labels)
 
     plt.show()
 
@@ -96,10 +96,10 @@ class DataPresenter():
     def render_optimal_cluster_figure(self):
         print(f"Optimal clusters for K={self.client.optimal_k}")
         print("Plotting PCA embeddings with K-means, K="+str(np.max(self.client.optimal_cluster_labels)+1))
-        self.generate_figure(coord_dataframe=self.client.projected_data, labels=self.client.optimal_cluster_labels)
+        self.generate_figure(coord_dataframe=self.client.projected_data, cluster_labels=self.client.optimal_cluster_labels)
 
-    def generate_figure(self, coord_dataframe, labels=None):
-        generate_figure(coord_dataframe=coord_dataframe, labels=labels)
+    def generate_figure(self, coord_dataframe, cluster_labels=None):
+        generate_figure(coord_dataframe=coord_dataframe, cluster_labels=cluster_labels)
 
     def generate_vote_heatmap(self, vote_df):
         sns.set_context('poster')
