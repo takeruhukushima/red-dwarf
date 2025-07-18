611259abae604237ddecc1e58d48aa041222489c
Patrick Connolly <patrick.c.connolly@gmail.com>
2025-07-18 00:13:28 -0500

    Fix tests.

--- tests/utils/clusterer/test_base.py
+++ tests/utils/clusterer/test_base.py
@@ -4,14 +4,14 @@ from reddwarf.utils.clusterer.base import run_clusterer
 from tests.fixtures import polis_convo_data
 from hdbscan import HDBSCAN
 
-@pytest.mark.parametrize("polis_convo_data", ["small"], indirect=True)
+@pytest.mark.parametrize("polis_convo_data", ["medium"], indirect=True)
 def test_run_clusterer_hdbscan(polis_convo_data):
     fixture = polis_convo_data
 
     X_math_data = np.asarray([
         fixture.math_data["base-clusters"]["x"],
         fixture.math_data["base-clusters"]["y"],
-    ])
+    ]).transpose()
 
     clusterer_model = run_clusterer(
         X_participants_clusterable=X_math_data,
@@ -22,6 +22,13 @@ def test_run_clusterer_hdbscan(polis_convo_data):
     assert isinstance(clusterer_model, HDBSCAN)
     assert hasattr(clusterer_model, "labels_")
 
+    n_samples = X_math_data.shape[0]
+    assert len(clusterer_model.labels_) == n_samples
+
+    # Ensure at least some are clustered.
+    label_counts = dict(zip(*np.unique(clusterer_model.labels_, return_counts=True)))
+    assert label_counts[-1] < len(clusterer_model.labels_)
+
 @pytest.mark.parametrize("polis_convo_data", ["small"], indirect=True)
 def test_run_clusterer_unknown(polis_convo_data):
     fixture = polis_convo_data
@@ -29,7 +36,7 @@ def test_run_clusterer_unknown(polis_convo_data):
     X_math_data = np.asarray([
         fixture.math_data["base-clusters"]["x"],
         fixture.math_data["base-clusters"]["y"],
-    ])
+    ]).transpose()
 
     with pytest.raises(NotImplementedError):
         clusterer_model = run_clusterer(
